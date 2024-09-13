e941436084422641a4d3a2c6daa934e4357999db
hyeonjjzm <hdjsjy0504@gmai>
2024-09-13 10:48:57 +0900

    09_13

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ export default function Laydout({children}){
     return(
         <>
         <Header></Header>
-        <Container fixed>
+        <Container fixed style={{margin:"20vh"}}>
         <div>{children}</div>
         </Container>
         <Footer></Footer>
